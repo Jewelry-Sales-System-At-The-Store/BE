504dfb506e9d13bc8423ba347e0e13af90b3587c
thangnguyen8520 <130530198+thangnguyen8520@users.noreply.github.com>
2024-07-02 15:16:17 +0700

    JewelryDashboard

--- Repositories/Interface/IBillJewelryRepository.cs
+++ Repositories/Interface/IBillJewelryRepository.cs
@@ -1,9 +1,14 @@
-﻿using BusinessObjects.Models;
+﻿using BusinessObjects.Dto.Dashboard;
+using BusinessObjects.Models;
 using Repositories.Interface.GenericRepository;
 
 namespace Repositories.Interface;
 
 public interface IBillJewelryRepository : ICreateRepository<BillJewelry>
 {
-    
+    Task<IEnumerable<BestSellingProductDto>> GetBestSellingProducts();
+    Task<IEnumerable<BestSellingProductTypeDto>> GetBestSellingProductTypes();
+    Task<IEnumerable<ProductRevenueDto>> GetTotalRevenueByProducts();
+    Task<IEnumerable<ProductTypeRevenueDto>> GetTotalRevenueByProductTypes();
+
 }
