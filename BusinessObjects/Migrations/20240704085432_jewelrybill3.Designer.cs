cbe9c4dfe4905369cc7f89347b6e008fc631fc8e
thangnguyen8520 <130530198+thangnguyen8520@users.noreply.github.com>
2024-07-04 15:58:45 +0700

    Fix db

--- BusinessObjects/Migrations/20240704085432_jewelrybill3.Designer.cs
+++ BusinessObjects/Migrations/20240704085432_jewelrybill3.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BusinessObjects.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace BusinessObjects.Migrations
 {
     [DbContext(typeof(JssatsContext))]
-    partial class JssatsContextModelSnapshot : ModelSnapshot
+    [Migration("20240704085432_jewelrybill3")]
+    partial class jewelrybill3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -76,7 +79,7 @@ namespace BusinessObjects.Migrations
                             CreatedAt = new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)),
                             CustomerId = "1",
                             IsPaid = false,
-                            SaleDate = new DateTimeOffset(new DateTime(2024, 7, 4, 15, 57, 47, 201, DateTimeKind.Unspecified).AddTicks(3329), new TimeSpan(0, 7, 0, 0, 0)),
+                            SaleDate = new DateTimeOffset(new DateTime(2024, 7, 4, 15, 54, 32, 454, DateTimeKind.Unspecified).AddTicks(948), new TimeSpan(0, 7, 0, 0, 0)),
                             TotalAmount = 500.0,
                             UpdatedAt = new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)),
                             UserId = "1"
@@ -88,7 +91,7 @@ namespace BusinessObjects.Migrations
                             CreatedAt = new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)),
                             CustomerId = "2",
                             IsPaid = false,
-                            SaleDate = new DateTimeOffset(new DateTime(2024, 7, 4, 15, 57, 47, 201, DateTimeKind.Unspecified).AddTicks(3332), new TimeSpan(0, 7, 0, 0, 0)),
+                            SaleDate = new DateTimeOffset(new DateTime(2024, 7, 4, 15, 54, 32, 454, DateTimeKind.Unspecified).AddTicks(952), new TimeSpan(0, 7, 0, 0, 0)),
                             TotalAmount = 1200.0,
                             UpdatedAt = new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)),
                             UserId = "2"
@@ -338,7 +341,7 @@ namespace BusinessObjects.Migrations
                             GemId = "1",
                             BuyPrice = 300f,
                             City = "Ha Noi",
-                            LastUpdated = new DateTimeOffset(new DateTime(2024, 7, 4, 15, 57, 47, 201, DateTimeKind.Unspecified).AddTicks(3384), new TimeSpan(0, 7, 0, 0, 0)),
+                            LastUpdated = new DateTimeOffset(new DateTime(2024, 7, 4, 15, 54, 32, 454, DateTimeKind.Unspecified).AddTicks(1017), new TimeSpan(0, 7, 0, 0, 0)),
                             SellPrice = 400f,
                             Type = "Ruby"
                         },
@@ -347,7 +350,7 @@ namespace BusinessObjects.Migrations
                             GemId = "2",
                             BuyPrice = 400f,
                             City = "Ha Noi",
-                            LastUpdated = new DateTimeOffset(new DateTime(2024, 7, 4, 15, 57, 47, 201, DateTimeKind.Unspecified).AddTicks(3388), new TimeSpan(0, 7, 0, 0, 0)),
+                            LastUpdated = new DateTimeOffset(new DateTime(2024, 7, 4, 15, 54, 32, 454, DateTimeKind.Unspecified).AddTicks(1020), new TimeSpan(0, 7, 0, 0, 0)),
                             SellPrice = 500f,
                             Type = "Sapphire"
                         },
@@ -356,7 +359,7 @@ namespace BusinessObjects.Migrations
                             GemId = "3",
                             BuyPrice = 500f,
                             City = "Ha Noi",
-                            LastUpdated = new DateTimeOffset(new DateTime(2024, 7, 4, 15, 57, 47, 201, DateTimeKind.Unspecified).AddTicks(3398), new TimeSpan(0, 7, 0, 0, 0)),
+                            LastUpdated = new DateTimeOffset(new DateTime(2024, 7, 4, 15, 54, 32, 454, DateTimeKind.Unspecified).AddTicks(1022), new TimeSpan(0, 7, 0, 0, 0)),
                             SellPrice = 600f,
                             Type = "Emerald"
                         });
@@ -395,7 +398,7 @@ namespace BusinessObjects.Migrations
                             GoldId = "1",
                             BuyPrice = 1000f,
                             City = "Ha Noi",
-                            LastUpdated = new DateTimeOffset(new DateTime(2024, 7, 4, 15, 57, 47, 201, DateTimeKind.Unspecified).AddTicks(3418), new TimeSpan(0, 7, 0, 0, 0)),
+                            LastUpdated = new DateTimeOffset(new DateTime(2024, 7, 4, 15, 54, 32, 454, DateTimeKind.Unspecified).AddTicks(1042), new TimeSpan(0, 7, 0, 0, 0)),
                             SellPrice = 1200f,
                             Type = "9999"
                         },
@@ -404,7 +407,7 @@ namespace BusinessObjects.Migrations
                             GoldId = "2",
                             BuyPrice = 1200f,
                             City = "Ha Noi",
-                            LastUpdated = new DateTimeOffset(new DateTime(2024, 7, 4, 15, 57, 47, 201, DateTimeKind.Unspecified).AddTicks(3421), new TimeSpan(0, 7, 0, 0, 0)),
+                            LastUpdated = new DateTimeOffset(new DateTime(2024, 7, 4, 15, 54, 32, 454, DateTimeKind.Unspecified).AddTicks(1044), new TimeSpan(0, 7, 0, 0, 0)),
                             SellPrice = 1400f,
                             Type = "SCJ"
                         },
@@ -413,7 +416,7 @@ namespace BusinessObjects.Migrations
                             GoldId = "3",
                             BuyPrice = 1400f,
                             City = "Ha Noi",
-                            LastUpdated = new DateTimeOffset(new DateTime(2024, 7, 4, 15, 57, 47, 201, DateTimeKind.Unspecified).AddTicks(3423), new TimeSpan(0, 7, 0, 0, 0)),
+                            LastUpdated = new DateTimeOffset(new DateTime(2024, 7, 4, 15, 54, 32, 454, DateTimeKind.Unspecified).AddTicks(1046), new TimeSpan(0, 7, 0, 0, 0)),
                             SellPrice = 1600f,
                             Type = "18k"
                         });
@@ -618,8 +621,8 @@ namespace BusinessObjects.Migrations
                             PromotionId = "1",
                             Description = "Giam gia 10%",
                             DiscountRate = 1.0,
-                            EndDate = new DateTimeOffset(new DateTime(2024, 7, 14, 15, 57, 47, 201, DateTimeKind.Unspecified).AddTicks(3296), new TimeSpan(0, 7, 0, 0, 0)),
-                            StartDate = new DateTimeOffset(new DateTime(2024, 7, 4, 15, 57, 47, 201, DateTimeKind.Unspecified).AddTicks(3268), new TimeSpan(0, 7, 0, 0, 0)),
+                            EndDate = new DateTimeOffset(new DateTime(2024, 7, 14, 15, 54, 32, 454, DateTimeKind.Unspecified).AddTicks(913), new TimeSpan(0, 7, 0, 0, 0)),
+                            StartDate = new DateTimeOffset(new DateTime(2024, 7, 4, 15, 54, 32, 454, DateTimeKind.Unspecified).AddTicks(882), new TimeSpan(0, 7, 0, 0, 0)),
                             Type = "Giam gia"
                         },
                         new
@@ -627,8 +630,8 @@ namespace BusinessObjects.Migrations
                             PromotionId = "2",
                             Description = "Giam gia 20%",
                             DiscountRate = 2.0,
-                            EndDate = new DateTimeOffset(new DateTime(2024, 7, 14, 15, 57, 47, 201, DateTimeKind.Unspecified).AddTicks(3303), new TimeSpan(0, 7, 0, 0, 0)),
-                            StartDate = new DateTimeOffset(new DateTime(2024, 7, 4, 15, 57, 47, 201, DateTimeKind.Unspecified).AddTicks(3302), new TimeSpan(0, 7, 0, 0, 0)),
+                            EndDate = new DateTimeOffset(new DateTime(2024, 7, 14, 15, 54, 32, 454, DateTimeKind.Unspecified).AddTicks(922), new TimeSpan(0, 7, 0, 0, 0)),
+                            StartDate = new DateTimeOffset(new DateTime(2024, 7, 4, 15, 54, 32, 454, DateTimeKind.Unspecified).AddTicks(921), new TimeSpan(0, 7, 0, 0, 0)),
                             Type = "Giam gia"
                         },
                         new
@@ -636,8 +639,8 @@ namespace BusinessObjects.Migrations
                             PromotionId = "3",
                             Description = "Giam gia 30%",
                             DiscountRate = 3.0,
-                            EndDate = new DateTimeOffset(new DateTime(2024, 7, 14, 15, 57, 47, 201, DateTimeKind.Unspecified).AddTicks(3307), new TimeSpan(0, 7, 0, 0, 0)),
-                            StartDate = new DateTimeOffset(new DateTime(2024, 7, 4, 15, 57, 47, 201, DateTimeKind.Unspecified).AddTicks(3306), new TimeSpan(0, 7, 0, 0, 0)),
+                            EndDate = new DateTimeOffset(new DateTime(2024, 7, 14, 15, 54, 32, 454, DateTimeKind.Unspecified).AddTicks(925), new TimeSpan(0, 7, 0, 0, 0)),
+                            StartDate = new DateTimeOffset(new DateTime(2024, 7, 4, 15, 54, 32, 454, DateTimeKind.Unspecified).AddTicks(924), new TimeSpan(0, 7, 0, 0, 0)),
                             Type = "Giam gia"
                         });
                 });
@@ -647,6 +650,9 @@ namespace BusinessObjects.Migrations
                     b.Property<string>("PurchaseId")
                         .HasMaxLength(7)
                         .HasColumnType("character varying(7)");
+
+                    b.Property<string>("BillJewelryId")
+                        .HasColumnType("text");
 
                     b.Property<string>("CustomerId")
                         .HasMaxLength(7)
@@ -686,7 +692,7 @@ namespace BusinessObjects.Migrations
                             CustomerId = "1",
                             IsBuyBack = 0,
                             JewelryId = "1",
-                            PurchaseDate = new DateTimeOffset(new DateTime(2024, 7, 4, 15, 57, 47, 201, DateTimeKind.Unspecified).AddTicks(3442), new TimeSpan(0, 7, 0, 0, 0)),
+                            PurchaseDate = new DateTimeOffset(new DateTime(2024, 7, 4, 15, 54, 32, 454, DateTimeKind.Unspecified).AddTicks(1066), new TimeSpan(0, 7, 0, 0, 0)),
                             PurchasePrice = 500.0,
                             UserId = "1"
                         },
@@ -696,7 +702,7 @@ namespace BusinessObjects.Migrations
                             CustomerId = "2",
                             IsBuyBack = 1,
                             JewelryId = "2",
-                            PurchaseDate = new DateTimeOffset(new DateTime(2024, 7, 4, 15, 57, 47, 201, DateTimeKind.Unspecified).AddTicks(3445), new TimeSpan(0, 7, 0, 0, 0)),
+                            PurchaseDate = new DateTimeOffset(new DateTime(2024, 7, 4, 15, 54, 32, 454, DateTimeKind.Unspecified).AddTicks(1068), new TimeSpan(0, 7, 0, 0, 0)),
                             PurchasePrice = 300.0,
                             UserId = "1"
                         });
---
+++ BusinessObjects/Migrations/20240704085747_jewelrybill5.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BusinessObjects.Migrations
+{
+    /// <inheritdoc />
+    public partial class jewelrybill5 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BillJewelryId",
+                table: "Purchases");
+
+            migrationBuilder.UpdateData(
+                table: "Bills",
+                keyColumn: "BillId",
+                keyValue: "1",
+                column: "SaleDate",
+                value: new DateTimeOffset(new DateTime(2024, 7, 4, 15, 57, 47, 201, DateTimeKind.Unspecified).AddTicks(3329), new TimeSpan(0, 7, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Bills",
+                keyColumn: "BillId",
+                keyValue: "2",
+                column: "SaleDate",
+                value: new DateTimeOffset(new DateTime(2024, 7, 4, 15, 57, 47, 201, DateTimeKind.Unspecified).AddTicks(3332), new TimeSpan(0, 7, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Gems",
+                keyColumn: "GemId",
+                keyValue: "1",
+                column: "LastUpdated",
+                value: new DateTimeOffset(new DateTime(2024, 7, 4, 15, 57, 47, 201, DateTimeKind.Unspecified).AddTicks(3384), new TimeSpan(0, 7, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Gems",
+                keyColumn: "GemId",
+                keyValue: "2",
+                column: "LastUpdated",
+                value: new DateTimeOffset(new DateTime(2024, 7, 4, 15, 57, 47, 201, DateTimeKind.Unspecified).AddTicks(3388), new TimeSpan(0, 7, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Gems",
+                keyColumn: "GemId",
+                keyValue: "3",
+                column: "LastUpdated",
+                value: new DateTimeOffset(new DateTime(2024, 7, 4, 15, 57, 47, 201, DateTimeKind.Unspecified).AddTicks(3398), new TimeSpan(0, 7, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Golds",
+                keyColumn: "GoldId",
+                keyValue: "1",
+                column: "LastUpdated",
+                value: new DateTimeOffset(new DateTime(2024, 7, 4, 15, 57, 47, 201, DateTimeKind.Unspecified).AddTicks(3418), new TimeSpan(0, 7, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Golds",
+                keyColumn: "GoldId",
+                keyValue: "2",
+                column: "LastUpdated",
+                value: new DateTimeOffset(new DateTime(2024, 7, 4, 15, 57, 47, 201, DateTimeKind.Unspecified).AddTicks(3421), new TimeSpan(0, 7, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Golds",
+                keyColumn: "GoldId",
+                keyValue: "3",
+                column: "LastUpdated",
+                value: new DateTimeOffset(new DateTime(2024, 7, 4, 15, 57, 47, 201, DateTimeKind.Unspecified).AddTicks(3423), new TimeSpan(0, 7, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Promotions",
+                keyColumn: "PromotionId",
+                keyValue: "1",
+                columns: new[] { "EndDate", "StartDate" },
+                values: new object[] { new DateTimeOffset(new DateTime(2024, 7, 14, 15, 57, 47, 201, DateTimeKind.Unspecified).AddTicks(3296), new TimeSpan(0, 7, 0, 0, 0)), new DateTimeOffset(new DateTime(2024, 7, 4, 15, 57, 47, 201, DateTimeKind.Unspecified).AddTicks(3268), new TimeSpan(0, 7, 0, 0, 0)) });
+
+            migrationBuilder.UpdateData(
+                table: "Promotions",
+                keyColumn: "PromotionId",
+                keyValue: "2",
+                columns: new[] { "EndDate", "StartDate" },
+                values: new object[] { new DateTimeOffset(new DateTime(2024, 7, 14, 15, 57, 47, 201, DateTimeKind.Unspecified).AddTicks(3303), new TimeSpan(0, 7, 0, 0, 0)), new DateTimeOffset(new DateTime(2024, 7, 4, 15, 57, 47, 201, DateTimeKind.Unspecified).AddTicks(3302), new TimeSpan(0, 7, 0, 0, 0)) });
+
+            migrationBuilder.UpdateData(
+                table: "Promotions",
+                keyColumn: "PromotionId",
+                keyValue: "3",
+                columns: new[] { "EndDate", "StartDate" },
+                values: new object[] { new DateTimeOffset(new DateTime(2024, 7, 14, 15, 57, 47, 201, DateTimeKind.Unspecified).AddTicks(3307), new TimeSpan(0, 7, 0, 0, 0)), new DateTimeOffset(new DateTime(2024, 7, 4, 15, 57, 47, 201, DateTimeKind.Unspecified).AddTicks(3306), new TimeSpan(0, 7, 0, 0, 0)) });
+
+            migrationBuilder.UpdateData(
+                table: "Purchases",
+                keyColumn: "PurchaseId",
+                keyValue: "1",
+                column: "PurchaseDate",
+                value: new DateTimeOffset(new DateTime(2024, 7, 4, 15, 57, 47, 201, DateTimeKind.Unspecified).AddTicks(3442), new TimeSpan(0, 7, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Purchases",
+                keyColumn: "PurchaseId",
+                keyValue: "2",
+                column: "PurchaseDate",
+                value: new DateTimeOffset(new DateTime(2024, 7, 4, 15, 57, 47, 201, DateTimeKind.Unspecified).AddTicks(3445), new TimeSpan(0, 7, 0, 0, 0)));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BillJewelryId",
+                table: "Purchases",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Bills",
+                keyColumn: "BillId",
+                keyValue: "1",
+                column: "SaleDate",
+                value: new DateTimeOffset(new DateTime(2024, 7, 4, 15, 56, 11, 166, DateTimeKind.Unspecified).AddTicks(8311), new TimeSpan(0, 7, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Bills",
+                keyColumn: "BillId",
+                keyValue: "2",
+                column: "SaleDate",
+                value: new DateTimeOffset(new DateTime(2024, 7, 4, 15, 56, 11, 166, DateTimeKind.Unspecified).AddTicks(8314), new TimeSpan(0, 7, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Gems",
+                keyColumn: "GemId",
+                keyValue: "1",
+                column: "LastUpdated",
+                value: new DateTimeOffset(new DateTime(2024, 7, 4, 15, 56, 11, 166, DateTimeKind.Unspecified).AddTicks(8373), new TimeSpan(0, 7, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Gems",
+                keyColumn: "GemId",
+                keyValue: "2",
+                column: "LastUpdated",
+                value: new DateTimeOffset(new DateTime(2024, 7, 4, 15, 56, 11, 166, DateTimeKind.Unspecified).AddTicks(8381), new TimeSpan(0, 7, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Gems",
+                keyColumn: "GemId",
+                keyValue: "3",
+                column: "LastUpdated",
+                value: new DateTimeOffset(new DateTime(2024, 7, 4, 15, 56, 11, 166, DateTimeKind.Unspecified).AddTicks(8383), new TimeSpan(0, 7, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Golds",
+                keyColumn: "GoldId",
+                keyValue: "1",
+                column: "LastUpdated",
+                value: new DateTimeOffset(new DateTime(2024, 7, 4, 15, 56, 11, 166, DateTimeKind.Unspecified).AddTicks(8404), new TimeSpan(0, 7, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Golds",
+                keyColumn: "GoldId",
+                keyValue: "2",
+                column: "LastUpdated",
+                value: new DateTimeOffset(new DateTime(2024, 7, 4, 15, 56, 11, 166, DateTimeKind.Unspecified).AddTicks(8407), new TimeSpan(0, 7, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Golds",
+                keyColumn: "GoldId",
+                keyValue: "3",
+                column: "LastUpdated",
+                value: new DateTimeOffset(new DateTime(2024, 7, 4, 15, 56, 11, 166, DateTimeKind.Unspecified).AddTicks(8409), new TimeSpan(0, 7, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Promotions",
+                keyColumn: "PromotionId",
+                keyValue: "1",
+                columns: new[] { "EndDate", "StartDate" },
+                values: new object[] { new DateTimeOffset(new DateTime(2024, 7, 14, 15, 56, 11, 166, DateTimeKind.Unspecified).AddTicks(8263), new TimeSpan(0, 7, 0, 0, 0)), new DateTimeOffset(new DateTime(2024, 7, 4, 15, 56, 11, 166, DateTimeKind.Unspecified).AddTicks(8234), new TimeSpan(0, 7, 0, 0, 0)) });
+
+            migrationBuilder.UpdateData(
+                table: "Promotions",
+                keyColumn: "PromotionId",
+                keyValue: "2",
+                columns: new[] { "EndDate", "StartDate" },
+                values: new object[] { new DateTimeOffset(new DateTime(2024, 7, 14, 15, 56, 11, 166, DateTimeKind.Unspecified).AddTicks(8273), new TimeSpan(0, 7, 0, 0, 0)), new DateTimeOffset(new DateTime(2024, 7, 4, 15, 56, 11, 166, DateTimeKind.Unspecified).AddTicks(8272), new TimeSpan(0, 7, 0, 0, 0)) });
+
+            migrationBuilder.UpdateData(
+                table: "Promotions",
+                keyColumn: "PromotionId",
+                keyValue: "3",
+                columns: new[] { "EndDate", "StartDate" },
+                values: new object[] { new DateTimeOffset(new DateTime(2024, 7, 14, 15, 56, 11, 166, DateTimeKind.Unspecified).AddTicks(8277), new TimeSpan(0, 7, 0, 0, 0)), new DateTimeOffset(new DateTime(2024, 7, 4, 15, 56, 11, 166, DateTimeKind.Unspecified).AddTicks(8275), new TimeSpan(0, 7, 0, 0, 0)) });
+
+            migrationBuilder.UpdateData(
+                table: "Purchases",
+                keyColumn: "PurchaseId",
+                keyValue: "1",
+                columns: new[] { "BillJewelryId", "PurchaseDate" },
+                values: new object[] { null, new DateTimeOffset(new DateTime(2024, 7, 4, 15, 56, 11, 166, DateTimeKind.Unspecified).AddTicks(8575), new TimeSpan(0, 7, 0, 0, 0)) });
+
+            migrationBuilder.UpdateData(
+                table: "Purchases",
+                keyColumn: "PurchaseId",
+                keyValue: "2",
+                columns: new[] { "BillJewelryId", "PurchaseDate" },
+                values: new object[] { null, new DateTimeOffset(new DateTime(2024, 7, 4, 15, 56, 11, 166, DateTimeKind.Unspecified).AddTicks(8578), new TimeSpan(0, 7, 0, 0, 0)) });
+        }
+    }
+}
