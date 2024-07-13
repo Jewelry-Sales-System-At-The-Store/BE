8ff81a6c4fdc90ea48e624afbf3a6be0325bc323
Nguyễn Trọng Nghĩa <nghia2852002@gmail.com>
2024-07-13 21:00:16 +0700

    remove pwd field

--- BusinessObjects/Dto/UserDto.cs
+++ BusinessObjects/Dto/UserDto.cs
@@ -13,8 +13,8 @@ public class UserDto
 
     public string? Email { get; set; }
 
-    public string? Password { get; set; }
+    //public string? Password { get; set; }
 
-    public bool Status { get; set; }
+    public bool? Status { get; set; }
     
 }
